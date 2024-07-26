3f7559e87f624891d56d6ddac794d6e0978dc7d3
ValeriaKornienko <valebog94@gmail.com>
2024-07-26 09:37:50 +0200

    Hemowrk16

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Homework15 from "./homeworks/Homework15/Homework15"
 import Lesson16 from "./lessons/lesson16/Lesson16"
+import Homework16 from "./homeworks/homework16/Homework16"
 
 
 function App() {
   return (
-    <><Lesson16 />
+    <><Homework16 />
     </>
   )
 }
